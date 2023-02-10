7f9a11e014ee7cefc027dcdc1f281cd2d891f646
dhrs-odoo <dhrs@odoo.com>
2023-02-10 16:58:25 +0530

    owl-useState-rps

--- rps-owl/app.js
+++ rps-owl/app.js
@@ -1,6 +1,4 @@
-const { Component, mount, xml, onMounted, onWillRender } = owl;
-let user_count=0
-let com_count=0
+const { Component, mount, xml, onMounted ,useState,props} = owl;
 
 class Scoreboard extends Component {
   static template = xml`
@@ -10,26 +8,28 @@ class Scoreboard extends Component {
         <div class="score-board">
             <div id="user" class="badge">User</div>
             <div id="computer" class="badge">Comp</div>
-            <span id="user_score">0</span> : <span id="computer_score">0</span>
+            <span id="user_score"><t t-esc = "props.state.user_count"/></span> : <span id="computer_score"><t t-esc = "props.state.com_count"/></span>
         </div>
     `;
+    static props = ["state"]
 }
 
 class Result extends Component {
   static template = xml`
     <div class="result">
-        <h3 id="user_result"></h3>
-        <h3 id="result"></h3>
-        <h3 id="com_result"></h3>
+        <h3 id="user_result"><t t-esc = "props.state.user_result"/></h3>
+        <h3 id="result"><t t-esc = "props.state.result"/></h3>
+        <h3 id="com_result"><t t-esc = "props.state.com_result"/></h3>
 
     </div>
     `;
+    static props = ["state"]
 }
 
 class Buttons extends Component {
   static template = xml`
-   <Scoreboard/>
-    <Result/>
+   <Scoreboard state="state"/>
+    <Result state="state"/>
     <div class="buttons">
     <div>
         <button class="button btn" id="rock" t-on-click="rockBtn">Rock</button>   
@@ -44,70 +44,74 @@ class Buttons extends Component {
 </div>
 
     `;
+
+    setup(){
+        this.state = useState({
+          result:"",
+          user_result:"",
+          com_result:"",
+          com_count:0,
+          user_count:0,  
+        })
+    }
     rockBtn(){
         let Computer_choice = (Math.floor(Math.random()*3+1))
         if(Computer_choice==2){
-            user_result.innerText="Rock"
-            result.innerText="You Lose"
-            com_result.innerText="PAPER"
-            com_count++ 
-            computer_score.innerText=com_count
+            this.state.user_result="Rock"
+            this.state.result="You Lose"
+            this.state.com_result="PAPER"
+            this.state.com_count++ 
         }
         else if(Computer_choice==3){
-            user_result.innerText="Rock"
-            result.innerText="You Win"
-            com_result.innerText="SCISSORS"
-            user_count++ 
-            user_score.innerText=user_count
+            this.state.user_result="Rock"
+            this.state.result="You Win"
+            this.state.com_result="SCISSORS"
+            this.state.user_count++ 
         }
         else{
-            user_result.innerText="ROCK"
-            result.innerText="It's a Tie"
-            com_result.innerText="ROCK"
+            this.state.user_result="ROCK"
+            this.state.result="It's a Tie"
+            this.state.com_result="ROCK"
         }
     }
     paperBtn(){
         let Computer_choice = (Math.floor(Math.random()*3+1))
         if(Computer_choice==1){
-            user_result.innerText="PAPER"
-            result.innerText="You Won"
-            com_result.innerText="ROCK"
-            user_count++ 
-            user_score.innerText=user_count
+            this.state.user_result="PAPER"
+            this.state.result="You Won"
+            this.state.com_result="ROCK"
+            this.state.user_count++ 
         }
         else if(Computer_choice==2){
-            user_result.innerText="PAPER"
-            result.innerText="It's a Tie"
-            com_result.innerText="PAPER"
+            this.state.user_result="PAPER"
+            this.state.result="It's a Tie"
+            this.state.com_result="PAPER"
         }
         else if(Computer_choice==3)    {
-            user_result.innerText="PAPER"
-            result.innerText="You Lose"
-            com_result.innerText="SCISSORS"
-            user_count++ 
-            user_score.innerText=user_count
+            this.state.user_result="PAPER"
+            this.state.result="You Lose"
+            this.state.com_result="SCISSORS"
+            this.state.com_count++ 
         }
     }
     scissorBtn(){
         let Computer_choice = (Math.floor(Math.random()*3+1))
         if(Computer_choice==1){
-            user_result.innerText="SCISSORS"
-            result.innerText="You Lose"
-            com_result.innerText="ROCK"
-            user_count++ 
-            user_score.innerText=user_count
+            this.state.user_result="SCISSORS"
+            this.state.result="You Lose"
+            this.state.com_result="ROCK"
+            this.state.com_count++ 
         }
         else if(Computer_choice==2){
-            user_result.innerText="SCISSORS"
-            result.innerText="You Won"
-            com_result.innerText="PAPER"
-            user_count++ 
-            user_score.innerText=user_count
+            this.state.user_result="SCISSORS"
+            this.state.result="You Won"
+            this.state.com_result="PAPER"
+            this.state.user_count++ 
         }
         else{
-            user_result.innerText="SCISSORS"
-            result.innerText="It's a Tie"
-            com_result.innerText="SCISSORS"
+            this.state.user_result="SCISSORS"
+            this.state.result="It's a Tie"
+            this.state.com_result="SCISSORS"
         }
     }
   static components = { Result ,Scoreboard };
